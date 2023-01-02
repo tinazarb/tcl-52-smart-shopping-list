afd9f9331264a4f26813f0a98c34660b0887ff05
fullybaked <david@fullybaked.co.uk>
2023-01-02 15:12:37 -0800

    Firebase setup

---
+++ src/api/configs.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyAZVQ1G9CBFXBCp0aNTwvt4ojle0JHX4",
+  authDomain: "tcl-52-smart-shopping-list.firebaseapp.com",
+  projectId: "tcl-52-smart-shopping-list",
+  storageBucket: "tcl-52-smart-shopping-list.appspot.com",
+  messagingSenderId: "380992563508",
+  appId: "1:380992563508:web:0a2c04044d26bd83b79d61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
