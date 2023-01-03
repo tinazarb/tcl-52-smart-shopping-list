5d55875ab6c6fe474d4e232b1f330ede00ac40ac
EJ Mason <13525251+mxmason@users.noreply.github.com>
2023-01-02 18:09:48 -0800

    hotfix: fix firebase config

--- src/api/config.js
+++ src/api/config.js
@@ -3,13 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'FILL_ME_IN',
-	authDomain: 'FILL_ME_IN',
-	databaseURL: 'FILL_ME_IN',
-	projectId: 'FILL_ME_IN',
-	storageBucket: 'FILL_ME_IN',
-	messagingSenderId: 'FILL_ME_IN',
-	appId: 'FILL_ME_IN',
+  apiKey: "AIzaSyDyAZVQ1G9CBFXBCp0aNTwvt4ojle0JHX4",
+  authDomain: "tcl-52-smart-shopping-list.firebaseapp.com",
+  projectId: "tcl-52-smart-shopping-list",
+  storageBucket: "tcl-52-smart-shopping-list.appspot.com",
+  messagingSenderId: "380992563508",
+  appId: "1:380992563508:web:0a2c04044d26bd83b79d61"
 };
 
 // Initialize Firebase
