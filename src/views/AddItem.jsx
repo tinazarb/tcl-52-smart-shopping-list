90eb9ba8e8138f9c167227ed36005f389cbc6476
Nicole Schnurr <leannexnicole@gmail.com>
2023-01-16 15:46:51 -0800

    created form and form logic, updated firebase

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,3 +1,102 @@
-export function AddItem() {
-	return <p>Hello from the <code>/add-item</code> page!</p>
+import { addItem } from '../api/firebase';
+import { useState } from 'react';
+// import { list } from "./List"
+
+export function AddItem({ listToken }) {
+	//itemName behaviour
+	const [itemName, setItemName] = useState('');
+
+	const [handleChangeItem] = (e) => {
+		e.preventDefault();
+		setItemName(e.target.value);
+	};
+
+	//buyNext radio behaviours
+	const soon = ['7'];
+	const kindOfSoon = ['14'];
+	const notSoon = ['30'];
+
+	const [nextPurchase, setNextPurchase] = useState(soon);
+
+	const [handleChange] = (e) => {
+		setNextPurchase(e.target.value);
+	};
+
+	//what happens when the form is submitted?
+	const [submissionYes, setSubmissionYes] = useState('');
+
+	const submitForm = (e) => {
+		e.preventDefault();
+		//if they don't put anything in
+		if (itemName === '') {
+			console.log('ladies, theydies and gentlemen, you need to submit a name');
+			return;
+		}
+
+		//define itemData
+		let itemData = {
+			itemName: itemName,
+			daysUntilNextPurchase: Number(nextPurchase),
+		};
+
+		//add item
+		const submission = addItem(listToken, itemData);
+
+		if (submission) {
+			setItemName('');
+			setNextPurchase(soon);
+			setSubmissionYes(`Tina says ${itemName} is on yo' list!`);
+		} else {
+			setSubmissionYes(
+				'Yo homie, the submission is not working! Try again, we promise we will try harder',
+			);
+		}
+	};
+
+	return (
+		<div>
+			<form onSubmit={submitForm}>
+				<div>
+					<label htmlFor="itemName">Item Name:</label>
+					<input
+						type="text"
+						name="itemName"
+						value={itemName}
+						onChange={handleChangeItem}
+					/>
+				</div>
+				<div>
+					<label htmlFor="buyAgain">How soon will you buy this again?</label>
+					<fieldset>
+						<label htmlFor="soon">Soon</label>
+						<input
+							type="radio"
+							id="soon"
+							value={soon}
+							checked={nextPurchase === soon}
+							onChange={handleChange}
+						/>
+						<label htmlFor="kindOfSoon">Kind Of Soon</label>
+						<input
+							type="radio"
+							id="kindOfSoon"
+							value={soon}
+							checked={nextPurchase === kindOfSoon}
+							onChange={handleChange}
+						/>
+						<label htmlFor="notSoon">Not Soon</label>
+						<input
+							type="radio"
+							id="notSoon"
+							value={soon}
+							checked={nextPurchase === notSoon}
+							onChange={handleChange}
+						/>
+					</fieldset>
+				</div>
+				<button type="submit">Add Item</button>
+			</form>
+			<p>{submissionYes}</p>
+		</div>
+	);
 }
