f2adce44b88a350115cd6954f0a50daaf494a4bf
Nicole Schnurr <leannexnicole@gmail.com>
2023-01-23 14:49:07 -0800

    Delete previous map of items

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,15 +1,40 @@
 import { ListItem } from '../components';
+import { useState } from 'react';
 
 export function List({ data }) {
+	const [searchedItem, setSearchedItem] = useState('');
+
+	function handleChange(e) {
+		setSearchedItem(e.target.value);
+	}
+
+	const filteredItems = data.filter((item) =>
+		item.name.toLowerCase().includes(searchedItem.toLowerCase()),
+	);
+
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+			<form>
+				<label htmlFor="filter">Filter Items:</label>
+				<input
+					id="filter"
+					type="search"
+					value={searchedItem}
+					onChange={handleChange}
+					placeholder="Start Typing here..."
+				/>
+			</form>
 			<ul>
-				{data.length
-					? data.map((item) => <ListItem key={item.id} name={item.name} />)
-					: ''}
+				{!filteredItems.length ? (
+					<p>It's not here!</p>
+				) : (
+					filteredItems.map((list) => {
+						return <ListItem name={list.name} key={list.id} />;
+					})
+				)}
 			</ul>
 		</>
 	);
