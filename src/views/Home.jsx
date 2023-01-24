0f76c4d02b112c6613866ef0032fb0efc39cd171
tinazarb <tinazarb@gmail.com>
2023-01-24 17:51:58 -0500

    firebase query and form functions

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,47 @@
 import './Home.css';
+import { useCallback, useState } from 'react';
+import { checkListToken } from '../api/firebase';
 
-export function Home({ handleNewToken }) {
+export function Home({ handleNewToken, setListToken }) {
+	const [token, setToken] = useState('');
+
+	const handleClick = useCallback(() => {
+		handleNewToken();
+	}, [handleNewToken]);
+
+	const handleFormChange = (e) => {
+		const value = e.target.value;
+		setToken(value);
+	};
+
+	const handleTokenSubmit = async (e) => {
+		e.preventDefault();
+
+		const checkTokenExists = await checkListToken(token);
+
+		if (checkTokenExists) {
+			setListToken(token);
+		} else {
+			alert('Could not find this list');
+		}
+	};
+
 	return (
 		<div className="Home">
 			<p>
 				Hello from the home (<code>/</code>) page.
 			</p>
-			<button onClick={handleNewToken}>Create a new list!</button>
+			<button onClick={handleClick}>Create a new list!</button>
 			<p> Want to join an existing list? </p>
-			<form>
+			<form onSubmit={handleTokenSubmit}>
 				<label htmlFor="token"> Enter Token:</label>
-				<input type="text" name="token" id="token"></input>
+				<input
+					type="text"
+					name="token"
+					id="token"
+					value={token}
+					onChange={handleFormChange}
+				/>
 				<button type="submit"> Join</button>
 			</form>
 		</div>
