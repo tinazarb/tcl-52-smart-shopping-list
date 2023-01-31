0c16f9b942006036a723c93b16a2d14cea0b3f58
Elizabeth <elisha.elizabeth@gmail.com>
2023-01-31 11:20:41 -0800

    added checkboxes to ListItem

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,5 +1,14 @@
 import './ListItem.css';
 
 export function ListItem({ name }) {
-	return <li className="ListItem">{name}</li>;
+	return (
+		<>
+			<li className="ListItem">
+				<input type="checkbox" className="ListItem-checkbox" id="listItem" />
+				<label htmlFor="listItem" className="ListItem-label">
+					{name}
+				</label>
+			</li>
+		</>
+	);
 }
