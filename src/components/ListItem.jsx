aae79a5bf83795663333837788bea56c950f56a3
Elizabeth <elisha.elizabeth@gmail.com>
2023-01-31 12:04:16 -0800

    pass data and listToken to ListItem

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,10 +1,39 @@
 import './ListItem.css';
+import { useState } from 'react';
+// import { updateItem } from '../api';
 
-export function ListItem({ name }) {
+export function ListItem({ name, data, listToken }) {
+	const [itemPurchased, setItemPurchased] = useState('');
+
+	console.log(data, 'data');
+	console.log(itemPurchased, 'itemPurchased');
+
+	const itemData = {
+		id: data.id,
+		dateLastPurchased: new Date(),
+		totalPurchases: 1,
+	};
+
+	const handleSelect = (e) => {
+		let isChecked = e.target.checked;
+
+		if (isChecked) {
+			setItemPurchased(itemData);
+			// updateItem(listToken, itemPurchased)
+		}
+	};
+
 	return (
 		<>
 			<li className="ListItem">
-				<input type="checkbox" className="ListItem-checkbox" id="listItem" />
+				<input
+					className="ListItem-checkbox"
+					type="checkbox"
+					id="listItem"
+					value={name}
+					checked={itemPurchased}
+					onChange={handleSelect}
+				/>
 				<label htmlFor="listItem" className="ListItem-label">
 					{name}
 				</label>
