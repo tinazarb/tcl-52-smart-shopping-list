2439a466393776c12c7bde3ab47ca79d725ad54f
Louie Knolle <knollelw@gmail.com>
2023-02-06 21:36:57 -0500

    take user input and check for duplicates in list/clear input on submit

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -3,9 +3,10 @@
 import { addItem } from '../api/firebase';
 import { useState } from 'react';
 
-export function AddItem({ listToken }) {
+export function AddItem({ listToken, data }) {
 	//itemName behaviour
 	const [itemName, setItemName] = useState('');
+	console.log(data);
 
 	const handleChangeItem = (e) => {
 		e.preventDefault();
@@ -23,6 +24,9 @@ export function AddItem({ listToken }) {
 		setNextPurchase(e.target.value);
 	};
 
+	// Existing Data DB List Items
+	const existingItems = data.map((item) => item.name.toLowerCase());
+
 	//what happens when the form is submitted?
 	const [submissionYes, setSubmissionYes] = useState('');
 
@@ -33,6 +37,15 @@ export function AddItem({ listToken }) {
 		// 	setSubmissionYes('The item name cannot be blank :|');
 		// 	return;
 		// }
+
+		// Checking if new list item already exists in DB, alert if already exists
+		for (let i = 0; i < existingItems.length; i++) {
+			if (itemName.toLowerCase() === existingItems[i]) {
+				alert(`${itemName} already exists in your list`);
+				setItemName('');
+				return;
+			}
+		}
 
 		//define itemData
 		let itemData = {
@@ -59,6 +72,7 @@ export function AddItem({ listToken }) {
 					<label htmlFor="itemName">
 						Item Name:
 						<input
+							id="addItemInput"
 							type="text"
 							name="itemName"
 							value={itemName}
