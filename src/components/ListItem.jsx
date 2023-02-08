929efb50a412bd0274c1138b3067f040f29f70d7
tinazarb <tinazarb@gmail.com>
2023-02-08 18:53:36 -0500

    implemented dateNextPurchased logic

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -16,50 +16,48 @@ export function ListItem({ name, data, listToken }) {
 		// toggling isChecked based on checkbox state
 		setIsChecked(nextChecked);
 		if (nextChecked) {
+			const today = new Date();
+			const ONE_DAY_IN_MILLISECONDS = 86400000;
+			// if the item hasn't been purchased, compare today to the date of its creation else compare today to the day it was last purchased.
+			const timeSinceLastPurchase =
+				data.dateLastPurchased === null
+					? getDaysBetweenDates(today.getTime(), data.dateCreated.toMillis())
+					: getDaysBetweenDates(
+							today.getTime(),
+							data.dateLastPurchased.toMillis(),
+					  );
+			console.log(timeSinceLastPurchase);
+
+			//interval last time
+			const lastEstimatedInterval =
+				data.dateLastPurchased === null
+					? getDaysBetweenDates(
+							data.dateNextPurchased.toMillis(),
+							data.dateCreated.toMillis(),
+					  )
+					: getDaysBetweenDates(
+							data.dateNextPurchased.toMillis(),
+							data.dateLastPurchased.toMillis(),
+					  );
+
+			//use calculateEstimate to tell the time until next purchase
+			const daysUntilNextPurchase = calculateEstimate(
+				lastEstimatedInterval,
+				timeSinceLastPurchase,
+				data.totalPurchases + 1,
+			);
+
 			const nextData = {
 				dateLastPurchased: new Date(),
 				totalPurchases: data.totalPurchases + 1,
-				// dateNextPurchased: new Date() + getDaysBetweenDates
+				dateNextPurchased: new Date(
+					today.getTime() + daysUntilNextPurchase * ONE_DAY_IN_MILLISECONDS,
+				),
 			};
 
 			updateItem(listToken, data.id, nextData);
 		}
 	};
-
-	const today = new Date();
-
-	// if the item hasn't been purchased, compare today to the date of its creation else compare today to the day it was last purchased.
-	const timeSinceLastPurchase =
-		data.dateLastPurchased === null
-			? getDaysBetweenDates(today.getTime(), data.dateCreated.toMillis())
-			: getDaysBetweenDates(today.getTime(), data.dateLastPurchased.toMillis());
-	console.log(timeSinceLastPurchase);
-
-	//interval last time
-	const lastEstimatedInterval =
-		data.dateLastPurchased === null
-			? getDaysBetweenDates(
-					data.dateNextPurchased.toMillis(),
-					data.dateCreated.toMillis(),
-			  )
-			: getDaysBetweenDates(
-					data.dateNextPurchased.toMillis(),
-					data.dateLastPurchased.toMillis(),
-			  );
-
-	//use calculateEstimate to tell the time until next purchase
-	const daysUntilNextPurchase = calculateEstimate(
-		lastEstimatedInterval,
-		timeSinceLastPurchase,
-		data.totalPurchases + 1,
-	);
-
-	console.log({
-		itemName: data.name,
-		timeSinceLastPurchase,
-		lastEstimatedInterval,
-		daysUntilNextPurchase,
-	});
 
 	return (
 		<>
