29324562a620a0291bd09c8bb4893b29c87831f4
tinazarb <tinazarb@gmail.com>
2023-02-08 18:32:12 -0500

    date math logic

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,6 +2,7 @@ import './ListItem.css';
 import { useState } from 'react';
 import { updateItem } from '../api';
 import { getDaysBetweenDates } from '../utils';
+import { calculateEstimate } from '@the-collab-lab/shopping-list-utils';
 
 export function ListItem({ name, data, listToken }) {
 	const initialChecked =
@@ -23,19 +24,42 @@ export function ListItem({ name, data, listToken }) {
 
 			updateItem(listToken, data.id, nextData);
 		}
-
-		const today = new Date();
-
-		// if the item hasn't been purchased, compare today to the date of its creation else compare today to the day it was last purchased.
-		const delta =
-			data.dateLastPurchased === null
-				? getDaysBetweenDates(today.getTime(), data.dateCreated.toMillis())
-				: getDaysBetweenDates(
-						today.getTime(),
-						data.dateLastPurchased.toMillis(),
-				  );
-		console.log(delta);
 	};
+
+	const today = new Date();
+
+	// if the item hasn't been purchased, compare today to the date of its creation else compare today to the day it was last purchased.
+	const timeSinceLastPurchase =
+		data.dateLastPurchased === null
+			? getDaysBetweenDates(today.getTime(), data.dateCreated.toMillis())
+			: getDaysBetweenDates(today.getTime(), data.dateLastPurchased.toMillis());
+	console.log(timeSinceLastPurchase);
+
+	//interval last time
+	const lastEstimatedInterval =
+		data.dateLastPurchased === null
+			? getDaysBetweenDates(
+					data.dateNextPurchased.toMillis(),
+					data.dateCreated.toMillis(),
+			  )
+			: getDaysBetweenDates(
+					data.dateNextPurchased.toMillis(),
+					data.dateLastPurchased.toMillis(),
+			  );
+
+	//use calculateEstimate to tell the time until next purchase
+	const daysUntilNextPurchase = calculateEstimate(
+		lastEstimatedInterval,
+		timeSinceLastPurchase,
+		data.totalPurchases + 1,
+	);
+
+	console.log({
+		itemName: data.name,
+		timeSinceLastPurchase,
+		lastEstimatedInterval,
+		daysUntilNextPurchase,
+	});
 
 	return (
 		<>
