a6195cff141da61d7430d71acf21004686c035ab
Elizabeth <elisha.elizabeth@gmail.com>
2023-03-02 18:53:19 -0800

    add copy to About page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,7 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 export function About() {
 	return (
 		<>
-			<h1>About page</h1>
+			{/* <h2>How to Use Your Smart Shopping List App</h2> */}
+			<h4>Accomplish the following using the Smart Shopping List App:</h4>
+			<ul>
+				<li>Create a new shopping list</li>
+				<li>Join an existing shopping list using a three word token</li>
+				<li>
+					Add new items to your list and indicate when it needs to be purchased
+					next
+				</li>
+				<li>Mark off items from your list when the item is purchased</li>
+				<li>Remove items from your list</li>
+				<li>Search for items on your list</li>
+				<li>
+					The app will determine and indicate when you should purchase an item
+					next
+				</li>
+			</ul>
+			<button>
+				<NavLink to="/">Return to home</NavLink>
+			</button>
 		</>
 	);
 }
