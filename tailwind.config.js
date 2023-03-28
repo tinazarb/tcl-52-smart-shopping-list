a084f926a2543d326e4a7c85fe2816b63308e55f
tinazarb <tinazarb@gmail.com>
2023-03-28 14:40:31 -0400

    updated with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				main: '#8ab301',
+				'main-light': '91bd00',
+				'main-dark': '#669c4e',
+				'main-darkest': '#558341',
+				charcoal: '#2a2933',
+				'medium-gray': '#dddce6',
+			},
+		},
+	},
+	plugins: [],
+};
