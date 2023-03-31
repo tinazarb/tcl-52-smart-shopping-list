aa5b631b5a0db68e2f6a2988d9192c89cb024972
tinazarb <tinazarb@gmail.com>
2023-03-31 15:25:08 -0400

    updated Layout with header and design

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,47 +1,68 @@
-import { Outlet } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
-import { faListDots } from '@fortawesome/free-solid-svg-icons';
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
-
-import './Layout.css';
+import { faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 export function Layout({ listToken }) {
+	const [copiedToken, setCopiedToken] = useState(false);
+
+	const copyTokenToClipboard = () => {
+		window.navigator.clipboard.writeText(listToken);
+		setCopiedToken(true);
+
+		setTimeout(() => {
+			setCopiedToken(false);
+		}, 3000);
+	};
+
+	const isHomeLocation = useLocation().pathname === '/';
+	const isAboutLocation = useLocation().pathname === '/about';
+
 	return (
-		<>
-			<div className="Layout">
-				{/* <header className="Layout-header">
-					<h1>Smart Shopping List</h1>
-				</header> */}
-				<main className="Layout-main">
-					<Outlet />
-				</main>
-				{listToken ? (
-					<nav className="Nav">
-						<div className="buttonContainer leftNav">
-							<NavLink to="/" className="Nav-link">
-								<FontAwesomeIcon icon={faHouse} />
-								<span>Home</span>
-							</NavLink>
-						</div>
-						<div className="buttonContainer leftNav">
-							<NavLink to="/list" className="Nav-link">
-								<FontAwesomeIcon icon={faListDots} />
-								<span>List</span>
-							</NavLink>
-						</div>
-						<div className="buttonContainer rightNav">
-							<NavLink to="add-item" className="Nav-link">
-								<FontAwesomeIcon icon={faCartPlus} />
-								<span>Add Item</span>
-							</NavLink>
-						</div>
-					</nav>
-				) : (
-					''
-				)}
-			</div>
-		</>
+		<div>
+			{!isHomeLocation && (
+				<header className="py-8">
+					<div className="flex items-start px-10">
+						<img src="/img/limey.png" alt="logo" className="w-10" />
+						<h1 className="font-medium text-3xl pl-2 mr-auto">Limey</h1>
+						{!isAboutLocation && (
+							<div className="">
+								<button
+									className="hover:text-main"
+									onClick={copyTokenToClipboard}
+									aria-describedby="share-button"
+								>
+									<FontAwesomeIcon
+										icon={faArrowUpFromBracket}
+										className="h-5 hover:text-main"
+									></FontAwesomeIcon>
+									<span className="icon mt-1 ml-1.5 text-m" id="share-button">
+										Share your list
+									</span>
+								</button>
+							</div>
+						)}
+					</div>
+					<div className="flex flex-col items-center">
+						{copiedToken && <p>Your unique token:</p>}
+						{copiedToken && (
+							<input
+								className="outline-0 py-2 px-4 bg-medium-gray text-sm rounded-3xl text-center"
+								type="text"
+								id="shareToken"
+								name="shareToken"
+								value={listToken}
+								aria-describedby="token-copied"
+								readOnly
+							/>
+						)}
+						{copiedToken && <p className="text-main">Copied to clipboard!</p>}
+					</div>
+				</header>
+			)}
+			<main className="Layout-main">
+				<Outlet />
+			</main>
+		</div>
 	);
 }
