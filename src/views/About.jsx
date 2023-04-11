4901fff61c894ed9c27fe54c2cb19ea71991e57d
tinazarb <tinazarb@gmail.com>
2023-04-11 18:53:47 -0400

    added mockup images to about page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -3,39 +3,59 @@ import { NavLink } from 'react-router-dom';
 
 export function About({ listToken }) {
 	return (
-		<div className="h-screen flex flex-col pl-20 pr-20 pb-20">
+		<div className="h-screen w-screen flex flex-col pl-20 pr-20 pb-20">
 			<img className="w-20 mb-5 self-center" src="/img/limey.png" alt="logo" />
 
 			<h1 className="text-4xl font-bold mb-10 self-center">
 				Learn How <strong className="text-main-darkest">Limey</strong> Works
 			</h1>
+			<img
+				src="img/limey-app-add-item.png"
+				className="sm:max-w-sm max-sm:max-w-xs self-center"
+				alt="adding items to Limey"
+			/>
 			<h2 className="mb-4 text-xl font-bold self-center">
 				Add groceries to your list
 			</h2>
-			<p className="mb-10">
+			<p className="mb-10 md:w-[40%] self-center">
 				Add items you regularly purchase to your list. When you add a new item,
 				Limey will ask you to estimate how soon you'll need to buy it again.
 			</p>
+			<img
+				src="img/limey-app-checkoff-items.png"
+				className="sm:max-w-sm max-sm:max-w-xs self-center"
+				alt="checking off purchased items on Limey"
+			/>
 			<h2 className="mb-4 text-xl font-bold self-center">
 				Check off items as you buy
 			</h2>
-			<p className="mb-10">
+			<p className="mb-10 md:w-[40%] self-center">
 				As you buy your groceries, check them off your list to help Limey learn
 				how often you need different items.
 			</p>
+			<img
+				src="img/limey-app-purchase-date.png"
+				className="sm:max-w-sm max-sm:max-w-xs self-center"
+				alt="future purchase date for item on Limey"
+			/>
 			<h2 className="mb-4 text-xl font-bold self-center">
 				Limey predicts your purchases
 			</h2>
-			<p className="mb-10 mb-10">
+			<p className="mb-10 mb-10 md:w-[40%] self-center">
 				Limey is a smart list that uses your buying history to predict what
 				you'll need to buy next. Items you are likely to need soon will rise to
 				the top of your list. Items you probably won't need for a while will
 				appear lower on the list.
 			</p>
+			<img
+				src="img/limey-app-share-list.png"
+				className="sm:max-w-sm max-sm:max-w-xs self-center"
+				alt="sharing list on Limey"
+			/>
 			<h2 className="mb-4 text-xl font-bold self-center">
 				Share your list with others!
 			</h2>
-			<p className="mb-1 mb-10">
+			<p className="mb-1 mb-10 md:w-[40%] self-center">
 				You can share the same list with everyone who shops for your household.
 				Easily provide your lists's three word token to a friend or family
 				member, and they can use it to join your list from Limey's home screen.
